e5f05e3ddf2cd80e7fd25720fe60d136271fa2fe
fraser-chapman <fraser.chapman@autotrader.co.uk>
2019-01-22 09:00:30 +0000

    added test to verify index method returns correct string

---
+++ src/test/java/hello/HelloControllerTest.java
@@ -0,0 +1,18 @@
+package hello;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloControllerTest {
+
+    private HelloController testSubject;
+
+    @Test
+    public void indexReturnsGreeting() {
+        testSubject = new HelloController();
+        Assertions.assertThat(testSubject.index()).isEqualTo("Greetings from Spring Boot");
+    }
+
+}
